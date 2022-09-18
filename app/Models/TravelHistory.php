71fdff27f04958eed2abfdd867a4abdff7f04ba4
Filipe Lukebana <filipelukebana@gmail.com>
2022-09-18 11:56:42 -0300

    🔥 Created Travel History Model and Migration

---
+++ app/Models/TravelHistory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TravelHistory extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'driver_id',
+        'passenger_id',
+        'start_location_id',
+        'destination_location_id',
+    ];
+}
---
+++ database/migrations/2022_09_18_145052_create_travel_histories_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTravelHistoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('travel_histories', function (Blueprint $table) {
+            $table->id();
+            $table->driver_id()->integer();
+            $table->passenger_id()->integer()->nullable();
+            $table->start_location_id()->integer();
+            $table->destination_location_id()->integer()->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('travel_histories');
+    }
+}
